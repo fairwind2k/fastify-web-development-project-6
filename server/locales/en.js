e68c9f93e3680c5874209c26370b41147b8b0367
fairwind2k <fairwind@md2k.net>
2026-04-12 02:07:53 +0200

    update locales

--- server/locales/en.js
+++ server/locales/en.js
@@ -18,6 +18,13 @@ export default {
           error: 'Failed to register',
           success: 'User registered successfully',
         },
+        update: {
+          error: 'Failed to update user',
+          success: 'User updated successfully',
+        },
+        delete: {
+          success: 'User deleted successfully',
+        },
       },
       authError: 'Access denied! Please login',
     },
@@ -39,10 +46,16 @@ export default {
       users: {
         id: 'ID',
         email: 'Email',
+        firstName: 'First name',
+        lastName: 'Last name',
         createdAt: 'Created at',
         new: {
           submit: 'Register',
           signUp: 'Register',
+        },
+        edit: {
+          title: 'Edit user',
+          submit: 'Save',
         },
       },
       welcome: {
