748e9db521968d97e364bd4eecd69ad694e43979
fairwind2k <fairwind@md2k.net>
2026-04-12 02:07:05 +0200

    add new fields in users table

--- server/migrations/20200716171734_create_users_table.js
+++ server/migrations/20200716171734_create_users_table.js
@@ -1,10 +1,12 @@
-// @ts-check
+// @ts-nocheck
 
 export const up = (knex) => (
   knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
     table.string('email');
     table.string('password_digest');
+    table.string('first_name').defaultTo('');
+    table.string('last_name').defaultTo('');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
