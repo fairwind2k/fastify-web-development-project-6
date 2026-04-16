15b7c804436fb248db0a712a39a7e37c4e731d51
fairwind2k <fairwind@md2k.net>
2026-04-17 00:39:01 +0200

    add generating data instead of fixtures

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -1,20 +1,49 @@
 // @ts-nocheck
 
-import { URL } from 'url';
-import fs from 'fs';
-import path from 'path';
+import { faker } from '@faker-js/faker';
+import encrypt from '../../server/lib/secure.cjs';
 
-// TODO: использовать для фикстур https://github.com/viglucci/simple-knex-fixtures
+export const buildUser = (params = {}) => ({
+  email: faker.internet.email(),
+  password: faker.internet.password({ length: 10 }),
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  ...params,
+});
 
-const getFixturePath = (filename) => path.join('..', '..', '__fixtures__', filename);
-const readFixture = (filename) => fs.readFileSync(new URL(getFixturePath(filename), import.meta.url), 'utf-8').trim();
-const getFixtureData = (filename) => JSON.parse(readFixture(filename));
+export const buildStatus = (params = {}) => ({
+  name: faker.word.noun(),
+  ...params,
+});
 
-export const getTestData = () => getFixtureData('testData.json');
-
-export const prepareData = async (app) => {
+export const createUser = async (app, params = {}) => {
   const { knex } = app.objection;
+  const userData = buildUser(params);
+  await knex('users').insert({
+    email: userData.email,
+    password_digest: encrypt(userData.password),
+    first_name: userData.firstName,
+    last_name: userData.lastName,
+  });
+  const user = await app.objection.models.user.query().findOne({ email: userData.email });
+  return { ...userData, id: user.id };
+};
 
-  // получаем данные из фикстур и заполняем БД
-  await knex('users').insert(getFixtureData('users.json'));
+export const createStatus = async (app, params = {}) => {
+  const { knex } = app.objection;
+  const statusData = buildStatus(params);
+  await knex('statuses').insert({ name: statusData.name });
+  const status = await app.objection.models.status.query().findOne({ name: statusData.name });
+  return { ...statusData, id: status.id };
+};
+
+export const signIn = async (app, { email, password }) => {
+  const response = await app.inject({
+    method: 'POST',
+    url: app.reverse('session'),
+    payload: { data: { email, password } },
+  });
+  const [sessionCookie] = response.cookies;
+  const { name, value } = sessionCookie;
+  return { [name]: value };
 };
