5c1835fb6b59fc527985fd370dc39b149f281070
fairwind2k <fairwind@md2k.net>
2026-04-17 00:26:58 +0200

    update users table

--- server/migrations/20200716171734_create_users_table.js
+++ server/migrations/20200716171734_create_users_table.js
@@ -1,15 +1,13 @@
 // @ts-nocheck
 
-export const up = (knex) => (
-  knex.schema.createTable('users', (table) => {
-    table.increments('id').primary();
-    table.string('email');
-    table.string('password_digest');
-    table.string('first_name').defaultTo('');
-    table.string('last_name').defaultTo('');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  })
-);
+export const up = (knex) => knex.schema.createTable('users', (table) => {
+  table.increments('id').primary();
+  table.string('email');
+  table.string('password_digest');
+  table.string('first_name').defaultTo('');
+  table.string('last_name').defaultTo('');
+  table.timestamp('created_at').defaultTo(knex.fn.now());
+  table.timestamp('updated_at').defaultTo(knex.fn.now());
+});
 
 export const down = (knex) => knex.schema.dropTable('users');
