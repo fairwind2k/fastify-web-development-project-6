f967df5361bc6cf063abca8bbf3a99068148db19
fairwind2k <fairwind@md2k.net>
2026-04-17 00:29:03 +0200

    create statuses table and routes

---
+++ server/routes/statuses.js
@@ -0,0 +1,56 @@
+import i18next from 'i18next';
+
+export default (app) => {
+  app
+    .get('/statuses', { name: 'statuses' }, async (req, reply) => {
+      const statuses = await app.objection.models.status.query();
+      reply.render('statuses/index', { statuses });
+      return reply;
+    })
+    .get('/statuses/new', { name: 'newStatus' }, (req, reply) => {
+      const status = new app.objection.models.status();
+      reply.render('statuses/new', { status });
+      return reply;
+    })
+    .get('/statuses/:id/edit', { name: 'editStatus' }, async (req, reply) => {
+      const status = await app.objection.models.status.query().findById(req.params.id);
+      reply.render('statuses/edit', { status });
+      return reply;
+    })
+    .post('/statuses', async (req, reply) => {
+      const status = new app.objection.models.status();
+      status.$set(req.body.data);
+
+      try {
+        const validStatus = await app.objection.models.status.fromJson(req.body.data);
+        await app.objection.models.status.query().insert(validStatus);
+        req.flash('info', i18next.t('flash.statuses.create.success'));
+        reply.redirect(app.reverse('statuses'));
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.statuses.create.error'));
+        reply.render('statuses/new', { status, errors: data });
+      }
+
+      return reply;
+    })
+    .patch('/statuses/:id', { name: 'status' }, async (req, reply) => {
+      const status = await app.objection.models.status.query().findById(req.params.id);
+
+      try {
+        await status.$query().patchAndFetch(req.body.data);
+        req.flash('info', i18next.t('flash.statuses.update.success'));
+        reply.redirect(app.reverse('statuses'));
+      } catch ({ data }) {
+        req.flash('error', i18next.t('flash.statuses.update.error'));
+        reply.render('statuses/edit', { status, errors: data });
+      }
+
+      return reply;
+    })
+    .delete('/statuses/:id', async (req, reply) => {
+      await app.objection.models.status.query().deleteById(req.params.id);
+      req.flash('info', i18next.t('flash.statuses.delete.success'));
+      reply.redirect(app.reverse('statuses'));
+      return reply;
+    });
+};
