2cab0da850ea147c290ba4520e745b869c338d63
fairwind2k <fairwind@md2k.net>
2026-04-19 00:35:09 +0200

    add tasks entity, models and routes

---
+++ server/migrations/20200716171736_create_tasks_table.js
@@ -0,0 +1,14 @@
+// @ts-nocheck
+
+export const up = (knex) => knex.schema.createTable('tasks', (table) => {
+  table.increments('id').primary();
+  table.string('name').notNullable();
+  table.text('description');
+  table.integer('status_id').references('id').inTable('statuses').notNullable();
+  table.integer('creator_id').references('id').inTable('users').notNullable();
+  table.integer('executor_id').references('id').inTable('users');
+  table.timestamp('created_at').defaultTo(knex.fn.now());
+  table.timestamp('updated_at').defaultTo(knex.fn.now());
+});
+
+export const down = (knex) => knex.schema.dropTable('tasks');
---
+++ server/models/Task.cjs
@@ -0,0 +1,46 @@
+
+const { Model } = require('objection');
+const BaseModel = require('./BaseModel.cjs');
+
+module.exports = class Task extends BaseModel {
+  static get tableName() {
+    return 'tasks';
+  }
+
+  static get relationMappings() {
+    const User = require('./User.cjs');
+    const Status = require('./Status.cjs');
+    return {
+      status: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Status,
+        join: { from: 'tasks.status_id', to: 'statuses.id' },
+      },
+      creator: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: { from: 'tasks.creator_id', to: 'users.id' },
+      },
+      executor: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: { from: 'tasks.executor_id', to: 'users.id' },
+      },
+    };
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name', 'statusId', 'creatorId'],
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string', minLength: 1 },
+        description: { type: 'string' },
+        statusId: { type: 'integer' },
+        creatorId: { type: 'integer' },
+        executorId: { type: 'integer' },
+      },
+    };
+  }
+}
