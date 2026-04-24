f62c9e552af65709c4dda666e7c67ea756aac263
fairwind2k <fairwind@md2k.net>
2026-04-24 02:50:44 +0200

    fix rollbar

--- rollbar.js
+++ rollbar.js
@@ -1,13 +1,15 @@
 import Rollbar from 'rollbar';
 
-const rollbar = new Rollbar({
-  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-  payload: {
-    code_version: '1.0.0',
-  },
-  environment: process.env.ROLLBAR_ENVIRONMENT,
-});
+const rollbar = process.env.ROLLBAR_ACCESS_TOKEN
+  ? new Rollbar({
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      code_version: '1.0.0',
+    },
+    environment: process.env.ROLLBAR_ENVIRONMENT,
+  })
+  : { error: () => {} };
 
 export default rollbar;
